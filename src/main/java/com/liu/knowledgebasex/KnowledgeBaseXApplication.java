3b4c0dc77e7f374a14e8614758e81ae83a25baf5
wiki <liutx24@gmail.com>
2025-04-25 14:19:37 +0800

    Initial project setup

---
+++ src/main/java/com/liu/knowledgebasex/KnowledgeBaseXApplication.java
@@ -0,0 +1,13 @@
+package com.liu.knowledgebasex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeBaseXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgeBaseXApplication.class, args);
+	}
+
+}
