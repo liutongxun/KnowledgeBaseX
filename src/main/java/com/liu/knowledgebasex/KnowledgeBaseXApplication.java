ac7aded29df0fa889504f57a288772a451d58467
liutongxun <liutx24@gmail.com>
2025-04-29 16:34:12 +0800

    integrate mybatis

--- src/main/java/com/liu/knowledgebasex/KnowledgeBaseXApplication.java
+++ src/main/java/com/liu/knowledgebasex/KnowledgeBaseXApplication.java
@@ -1,9 +1,13 @@
 package com.liu.knowledgebasex;
 
+import org.apache.commons.logging.Log;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
+@MapperScan("com.liu.knowledgebasex.mapper")
 public class KnowledgeBaseXApplication {
 
 	public static void main(String[] args) {
